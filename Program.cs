40d6ca9264f75132f81fc2e2437407bc56e2c886
Luciano Bueno <luciano.bueno@ingacode.com.br>
2024-09-18 16:33:47 -0300

    feat: completa desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Utilizando o espertofone Iphone");
+Smartphone iphone = new Iphone(numero: "41 98828-1937", modelo: "15", imei: "123456789", memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("------------------------");
+
+Console.WriteLine("Utilizando o burrofone Nokia");
+Smartphone nokia = new Nokia(numero: "41 98828-1938", modelo: "Tijolo", imei: "987654321", memoria: 2);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Cobrinha");
+
+Console.ReadLine();
